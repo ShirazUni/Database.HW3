f34bb3501854e7bb9f050b47b476601d5ea65795
Hamid Reza Ashkiyan <HamidRezaAshkiyan@gmail.com>
2021-07-10 12:02:32 +0430

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+Console.WriteLine("Enter RDF URL:");
+
+var graph = new Graph();
+UriLoader.Load(graph, new Uri(Console.ReadLine()));
+
+var sb = new StringBuilder();
+foreach (var triple in graph.Triples)
+    sb.AppendLine(triple.ToString());
+
+Console.WriteLine(sb.ToString());
+Console.ReadKey();
